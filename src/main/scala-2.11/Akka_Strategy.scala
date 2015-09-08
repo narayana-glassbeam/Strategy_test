2e09e600da70651a4dbae002ad946aa4edfe544f
narayana1208 <narayana.reddy@glassbeam.com>
2015-09-08 20:43:36 +0530

    Added Grandchild Count to see escalate Exception Strategy of Akk

--- src/main/scala-2.11/Akka_Strategy.scala
+++ src/main/scala-2.11/Akka_Strategy.scala
@@ -3,45 +3,64 @@ import akka.actor.Actor
 import akka.actor.ActorRef
 import akka.actor.OneForOneStrategy
 import akka.actor.Props
+import akka.actor.SupervisorStrategy.Escalate
 import akka.actor.SupervisorStrategy.Restart
+import akka.actor.SupervisorStrategy.Resume
 import akka.actor.SupervisorStrategy.Stop
 import akka.actor.Terminated
 
 import scala.concurrent.duration._
 
 class ChildCount extends Actor{
-	/*override val supervisorStrategy =
+	override val supervisorStrategy =
 		OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 60 seconds) {
-			case _: Exception => Stop // there are no 'fatal' exceptions in the parsing stage
-		}*/
+			case _:Exception => Escalate // there are no 'fatal' exceptions in the parsing stage
+		}
+
+	val grandChildCount:ActorRef = context.actorOf(Props[GrandChildCount],"GrandChildCount")
 
 	def receive = {
 
 		case cc:Int =>
-
-			if(cc==3)
-				{
+			if(cc==3) {
 					println("In child Throwing Arithmetic Exception for value "+cc+" and restarting child")
 					throw new ArithmeticException()
 				}
-			else if(cc==6)
-				{
-					println("In Child Count "+cc+" Throwing General Exception and stopping ")
-					throw new Exception
+			else if(cc==6) {
+					println("In Child Count "+cc+" Throwing Null Pointer Exception and Resuming childcount actor ")
+					throw new NullPointerException
 				}
-			else
-				{
+			else {
 					println("In Child Count "+cc)
+				grandChildCount ! cc
 				}
 
 		case Terminated(x) => println(s"child actor $x dead")
 	}
 }
+class GrandChildCount extends Actor{
+
+	def receive = {
+		case gcc:Int =>
+			if(gcc==8)
+			{
+				println("In Grand child Escalating Exception to ChildCount actor to stop receiving further data ")
+				throw new Exception
+			}
+			else{
+				println("In Grand Child Count "+gcc)
+			}
+	}
+
+}
+
+
 class SupervisorCount extends Actor{
 
 	override val supervisorStrategy =
 		OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 60 seconds) {
 			case _: ArithmeticException => Restart
+				case _:NullPointerException => Resume
 			case _: Exception => Stop // there are no 'fatal' exceptions in the parsing stage
 		}
 
