9feb735462f8dbc088aa7d663e30657291db231a
narayana1208 <narayana.reddy@glassbeam.com>
2015-09-08 21:24:19 +0530

    added bharath concept of supervison Akka_strategy_1.scala

--- src/main/scala-2.11/Akka_Strategy_1.scala
+++ src/main/scala-2.11/Akka_Strategy_1.scala
@@ -1,43 +1,41 @@
-import _root_.akka.actor.ActorSystem
 import akka.actor.Actor
 import akka.actor.ActorRef
+import akka.actor.ActorSystem
 import akka.actor.OneForOneStrategy
 import akka.actor.Props
-import akka.actor.SupervisorStrategy.Escalate
-import akka.actor.SupervisorStrategy.Restart
-import akka.actor.SupervisorStrategy.Resume
+import akka.actor.SupervisorStrategy.Stop
 import akka.actor.Terminated
 
 import scala.concurrent.duration._
 
-class ChildCount extends Actor{
+class ChildCount_1 extends Actor{
 	override val supervisorStrategy =
 		OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 60 seconds) {
-			case _:Exception => Escalate // there are no 'fatal' exceptions in the parsing stage
+			case _:Exception => Stop // there are no 'fatal' exceptions in the parsing stage
 		}
 
-	val grandChildCount:ActorRef = context.actorOf(Props[GrandChildCount],"GrandChildCount")
+	//val grandChildCount:ActorRef = context.actorOf(Props[GrandChildCount],"GrandChildCount")
 
 	def receive = {
 
 		case cc:Int =>
 			if(cc==3) {
-					println("In child Throwing Arithmetic Exception for value "+cc+" and restarting child")
-					throw new ArithmeticException()
-				}
+				println("In child Throwing Arithmetic Exception for value "+cc+" and restarting child")
+				throw new ArithmeticException()
+			}
 			else if(cc==6) {
-					println("In Child Count "+cc+" Throwing Null Pointer Exception and Resuming childcount actor ")
-					throw new NullPointerException
-				}
+				println("In Child Count "+cc+" Throwing Null Pointer Exception and Resuming childcount actor ")
+				throw new NullPointerException
+			}
 			else {
-					println("In Child Count "+cc)
-				grandChildCount ! cc
-				}
+				println("In Child Count "+cc)
+				//grandChildCount ! cc
+			}
 
 		case Terminated(x) => println(s"child actor $x dead")
 	}
 }
-class GrandChildCount extends Actor{
+/*class GrandChildCount extends Actor{
 
 	def receive = {
 		case gcc:Int =>
@@ -51,20 +49,20 @@ class GrandChildCount extends Actor{
 			}
 	}
 
-}
+}*/
 
 
-class SupervisorCount extends Actor{
+class SupervisorCount_1 extends Actor{
 
-	override val supervisorStrategy =
+	/*override val supervisorStrategy =
 		OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 60 seconds) {
 			case _: ArithmeticException => Restart
-				case _:NullPointerException => Resume
+			case _:NullPointerException => Resume
 			case _: Exception => Resume // there are no 'fatal' exceptions in the parsing stage
-		}
+		}*/
 
 
-	val childCount:ActorRef = context.actorOf(Props[ChildCount],"ChildCount")
+	val childCount:ActorRef = context.actorOf(Props[ChildCount_1],"ChildCount")
 
 	def receive = {
 		case "send" =>
@@ -80,10 +78,10 @@ class SupervisorCount extends Actor{
 }
 
 
-object TestSupervision{
+object AkkaSupervision{
 
 	val system = ActorSystem("Akka_Strategy")
-	val supervisorCount = system.actorOf(Props[SupervisorCount], "SupervisorCount")
+	val supervisorCount = system.actorOf(Props[SupervisorCount_1], "SupervisorCount")
 
 	def main(args: Array[String]): Unit = {
 
